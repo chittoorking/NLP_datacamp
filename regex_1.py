2c4976f2d125978503edfc318820927cb3ff18e6
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-06-04 21:16:16 +0530

    Create regex_1.py

---
+++ regex_1.py
@@ -0,0 +1,29 @@
+#Split my_string on each sentence ending. 
+#To do this:
+#Write a pattern called sentence_endings to match sentence endings (.?!).
+#Use re.split() to split my_string on the pattern and print the result.
+#Find and print all capitalized words in my_string by writing a pattern called capitalized_words and using re.findall().
+#Remember the [a-z] pattern shown in the video to match lowercase groups? Modify that pattern appropriately in order to match uppercase groups.
+#Write a pattern called spaces to match one or more spaces ("\s+") and then use re.split() to split my_string on this pattern, keeping all punctuation intact. Print the result.
+#Find all digits in my_string by writing a pattern called digits ("\d+") and using re.findall(). Print the result.
+# Write a pattern to match sentence endings: sentence_endings
+sentence_endings = r"[.?!]"
+
+# Split my_string on sentence endings and print the result
+print(re.split(sentence_endings,my_string))
+
+# Find all capitalized words in my_string and print the result
+capitalized_words = r"[A-Z]\w+"
+print(re.findall(capitalized_words,my_string))
+
+# Split my_string on spaces and print the result
+spaces = r"\s+"
+print(re.split(spaces,my_string))
+
+# Find all digits in my_string and print the result
+digits = r"\d+"
+print(re.findall(digits, my_string))
+#["Let's write RegEx", "  Won't that be fun", '  I sure think so', '  Can you find 4 sentences', '  Or perhaps, all 19 words', '']
+#['Let', 'RegEx', 'Won', 'Can', 'Or']
+#["Let's", 'write', 'RegEx!', "Won't", 'that', 'be', 'fun?', 'I', 'sure', 'think', 'so.', 'Can', 'you', 'find', '4', 'sentences?', 'Or', 'perhaps,', 'all', '19', 'words?']
+#['4', '19']
