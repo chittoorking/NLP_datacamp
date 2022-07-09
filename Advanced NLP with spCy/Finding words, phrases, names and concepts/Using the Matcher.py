f93ff0414aa569e585a02b57bbefda1baa6011ee
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-07-09 15:39:33 +0530

    Create Using the Matcher.py

---
+++ Advanced NLP with spCy/Finding words, phrases, names and concepts/Using the Matcher.py
@@ -0,0 +1,13 @@
+# Import the Matcher
+from spacy.matcher import Matcher
+
+# Initialize the Matcher with the shared vocabulary
+matcher = Matcher(nlp.vocab)
+# Create a pattern matching two tokens: "iPhone" and "X"
+pattern = [{'TEXT':"iPhone"},{'TEXT':"X"}]
+
+# Add the pattern to the matcher
+matcher.add('IPHONE_X_PATTERN', None, pattern)
+# Use the matcher on the doc
+matches = matcher(doc)
+print('Matches:', [doc[start:end].text for match_id, start, end in matches])
