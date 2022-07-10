77aaeedbfa21abc73f0f327f00bb28990c7ca97d
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-07-10 09:31:53 +0530

    Create Strings to hashes.py

---
+++ Advanced NLP with spCy/Large scale dataanalysis with spaCy/Strings to hashes.py
@@ -0,0 +1,7 @@
+# Look up the hash for the word "cat"
+cat_hash = nlp.vocab.strings["cat"]
+print(cat_hash)
+
+# Look up the cat_hash to get the string
+cat_string = nlp.vocab.strings[cat_hash]
+print(cat_string)
