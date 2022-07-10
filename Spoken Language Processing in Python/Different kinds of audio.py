946337cc2d9a78e01333505613a12912689e8e40
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-07-10 23:19:32 +0530

    Update Different kinds of audio.py

--- Spoken Language Processing in Python/Different kinds of audio.py
+++ Spoken Language Processing in Python/Different kinds of audio.py
@@ -1,4 +1,10 @@
+# Pass the Japanese version of good morning (japanese_audio) to recognize_google() using "en-US" as the language.
 
+# Pass the same Japanese audio (japanese_audio) using "ja" as the language parameter. Do you see a difference?
+
+# What about about non-speech audio? Pass leopard_audio to recognize_google() with show_all as True.
+
+# What if your speech files have non-audible human sounds? Pass charlie_audio to recognize_google() to find out.
 
 
 # Create a recognizer class
