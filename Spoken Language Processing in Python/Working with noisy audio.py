7cd3c87aa3bbbe9a503aca8d70893ff3ccba9702
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-07-10 23:35:08 +0530

    Update Working with noisy audio.py

--- Spoken Language Processing in Python/Working with noisy audio.py
+++ Spoken Language Processing in Python/Working with noisy audio.py
@@ -11,3 +11,51 @@ text = recognizer.recognize_google(clean_support_call_audio,
 					   language="en-US")
 
 print(text)
+
+
+# Let's do the same as before but with a noisy audio file saved as noisy_support_call and show_all parameter as True.
+recognizer = sr.Recognizer()
+
+# Record the audio from the noisy support call
+with noisy_support_call as source:
+  noisy_support_call_audio = recognizer.record(source)
+
+# Transcribe the speech from the noisy support call
+text = recognizer.recognize_google(noisy_support_call_audio,
+                         language="en-US",
+                         show_all=True)
+
+print(text)
+# Set the duration parameter of adjust_for_ambient_noise() to 1 (second) so recognizer adjusts for background noise.
+
+recognizer = sr.Recognizer()
+
+# Record the audio from the noisy support call
+with noisy_support_call as source:
+	# Adjust the recognizer energy threshold for ambient noise
+    recognizer.adjust_for_ambient_noise(source, duration=1.0)
+    noisy_support_call_audio = recognizer.record(noisy_support_call)
+ 
+# Transcribe the speech from the noisy support call
+text = recognizer.recognize_google(noisy_support_call_audio,
+                                   language="en-US",
+                                   show_all=True)
+
+print(text)
+
+# A duration of 1 was too long and it cut off some of the audio. Try setting duration to 0.5.
+
+recognizer = sr.Recognizer()
+
+# Record the audio from the noisy support call
+with noisy_support_call as source:
+	# Adjust the recognizer energy threshold for ambient noise
+    recognizer.adjust_for_ambient_noise(source, duration=0.5)
+    noisy_support_call_audio = recognizer.record(noisy_support_call)
+ 
+# Transcribe the speech from the noisy support call
+text = recognizer.recognize_google(noisy_support_call_audio,
+                                   language="en-US",
+                                   show_all=True)
+
+print(text)
