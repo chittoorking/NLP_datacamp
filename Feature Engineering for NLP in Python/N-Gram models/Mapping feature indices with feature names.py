1f6dfd7c5e4f375a4c4e73927920430e993104dd
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-07-15 21:53:04 +0530

    Create Mapping feature indices with feature names.py

---
+++ Feature Engineering for NLP in Python/N-Gram models/Mapping feature indices with feature names.py
@@ -0,0 +1,14 @@
+# Create CountVectorizer object
+vectorizer = CountVectorizer()
+
+# Generate matrix of word vectors
+bow_matrix = vectorizer.fit_transform(corpus)
+
+# Convert bow_matrix into a DataFrame
+bow_df = pd.DataFrame(bow_matrix.toarray())
+
+# Map the column names to vocabulary 
+bow_df.columns = vectorizer.get_feature_names()
+
+# Print bow_df
+print(bow_df)
