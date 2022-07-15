cdc32e69aff7ada09f961793e5fd3f97596f518b
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-07-15 20:33:28 +0530

    Create Lemmatizing the Gettysburg address.py

---
+++ Feature Engineering for NLP in Python/basic features and readability scores/Lemmatizing the Gettysburg address.py
@@ -0,0 +1,16 @@
+# Print the gettysburg address
+print(gettysburg)
+
+import spacy
+
+# Load the en_core_web_sm model
+nlp = spacy.load('en_core_web_sm')
+
+# Create a Doc object
+doc = nlp(gettysburg)
+
+# Generate lemmas
+lemmas = [token.lemma_ for token in doc]
+
+# Convert lemmas into a string
+print(' '.join(lemmas))
