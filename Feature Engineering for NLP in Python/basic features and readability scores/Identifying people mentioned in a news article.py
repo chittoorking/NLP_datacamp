c3a6d9995365dcbc78b2652afdb405e79367acc9
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-07-15 21:31:00 +0530

    Create Identifying people mentioned in a news article.py

---
+++ Feature Engineering for NLP in Python/basic features and readability scores/Identifying people mentioned in a news article.py
@@ -0,0 +1,11 @@
+def find_persons(text):
+  # Create Doc object
+  doc = nlp(text)
+  
+  # Identify the persons
+  persons = [ent.text for ent in doc.ents if ent.label_ == 'PERSON']
+  
+  # Return persons
+  return persons
+
+print(find_persons(tc))
