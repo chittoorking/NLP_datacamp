fed39a2f62f18038ee0e5011f6ff86887ad73cdc
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-07-15 20:07:08 +0530

    Update Hashtags and mentions in Russian tweets.py

--- Feature Engineering for NLP in Python/basic features and readability scores/Hashtags and mentions in Russian tweets.py
+++ Feature Engineering for NLP in Python/basic features and readability scores/Hashtags and mentions in Russian tweets.py
@@ -13,3 +13,23 @@ tweets['hashtag_count'] = tweets['content'].apply(count_hashtags)
 tweets['hashtag_count'].hist()
 plt.title('Hashtag count distribution')
 plt.show()
+
+
+
+
+# Function that returns number of mentions in a string
+def count_mentions(string):
+	# Split the string into words
+    words = string.split()
+    
+    # Create a list of words that are mentions
+    mentions = [word for word in words if word.startswith("@")]
+    
+    # Return number of mentions
+    return(len(mentions))
+
+# Create a feature mention_count and display distribution
+tweets['mention_count'] = tweets['content'].apply(count_mentions)
+tweets['mention_count'].hist()
+plt.title('Mention count distribution')
+plt.show()
